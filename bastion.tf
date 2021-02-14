f1927bdeba6f377f51e560f82955ecde16bb3b71
Francesco Furlan <francesco.furlan@molo17.com>
2021-02-14 23:12:06 +0100

    feat: add bastion

---
+++ bastion.tf
@@ -0,0 +1,61 @@
+resource "aws_key_pair" "bastion" {
+  key_name   = "bastion_key"
+  public_key = file("bastion_key.pub")
+  lifecycle {
+    ignore_changes = [public_key]
+  }
+}
+
+data "template_file" "wordpress_setup" {
+  template = file("scripts/wordpress_setup.sh")
+
+  vars = {
+    DB_PORT     = aws_db_instance.main.port
+    DB_HOST     = aws_db_instance.main.address
+    DB_USER     = var.db_username
+    DB_PASSWORD = var.db_password
+    DB_NAME     = var.db_name
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami                         = "ami-00aa4671cbf840d82"
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.bastion.key_name
+  subnet_id                   = element(module.networking.public_subnets, 0)
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
+
+  tags = {
+    Name    = "${var.project}-bastion"
+    Project = var.project
+    Env     = var.env
+  }
+}
+
+resource "aws_security_group" "bastion" {
+  name   = "${var.project}-bastion"
+  vpc_id = module.networking.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "${var.project}-bastion"
+    Project = var.project
+  }
+}
