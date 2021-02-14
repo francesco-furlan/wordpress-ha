c9760d904719eed3bc5a1b513ee08fe636210c5e
Francesco Furlan <francesco.furlan@molo17.com>
2021-02-14 22:33:56 +0100

    chore: add public_subnets to networking output

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -7,3 +7,8 @@ output "private_subnets" {
   description = "List of IDs of private subnets"
   value       = module.vpc.private_subnets
 }
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
